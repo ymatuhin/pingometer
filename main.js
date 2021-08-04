1f6a025ba884539431e3ee07b8e66379321c5913
ymatuhin <ym@ymatuhin.ru>
2021-08-04 23:18:47 +0300

    First release

--- main.js
+++ main.js
@@ -1,43 +1,69 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, Tray, Menu } = require("electron");
+const ping = require("ping");
+const fs = require("fs");
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
+let tray = null;
+let url = getPingUrl();
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  if (app.dock) app.dock.hide();
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  tray = new Tray("/Users/ym/Dev/ping/transparent.png");
+  tray.setTitle("∞");
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  const contextMenu = Menu.buildFromTemplate([
+    // {
+    //   label: "Hello!",
+    //   click() {
+    //     dialog.showMessageBoxSync({ title: "132" });
+    //   },
+    // },
+    { role: "quit" },
+  ]);
+  tray.setContextMenu(contextMenu);
+
+  runPing();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+const parseNumber = (val) => Math.round(Number(val));
+async function runPing() {
+  const params = {
+    timeout: 1,
+    min_reply: 10,
+    extra: ["-i", "0.2"],
+  };
+  let { avg, max, min, packetLoss, alive } = await ping.promise.probe(url, params);
+
+  avg = parseNumber(avg);
+  max = parseNumber(max);
+  min = parseNumber(min);
+  loss = parseNumber(packetLoss);
+
+  if (alive) {
+    const delta = Math.round((max - min) / 2);
+    const lossStr = loss > 0 ? `${loss}% ` : "";
+    const result = `${lossStr}${parseNumber(avg)}ms Δ${delta}ms`;
+    tray.setTitle(result);
+  } else {
+    tray.setTitle("offline");
+  }
+
+  setTimeout(runPing, 200);
+}
+
+function getPingUrl() {
+  try {
+    if (fs.existsSync(path)) {
+      const url = fs.readFileSync("~/.ping-ping-ping") || "1.1.1.1";
+      return url;
+    }
+  } catch (err) {
+    return "1.1.1.1";
+  }
+}
